c3ea81c7df662c885f9e912207dd62e2c4805d58
konrad920 <128093250+konrad920@users.noreply.github.com>
2025-05-17 14:48:38 +0200

    instrukcja 2 - zadanie 1.5 parametryczny test

--- src/test/java/pl/wsiz/view/UserListViewTest.java
+++ src/test/java/pl/wsiz/view/UserListViewTest.java
@@ -1,6 +1,8 @@
 package pl.wsiz.view;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,4 +16,20 @@ class UserListViewTest {
 
         assertEquals("Andrzej             ", textWithSpaces);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Andrzej", "Konrad", "Stanisława"})
+    void withSpacesParameterized(String name) {
+
+        UserListView userListView = new UserListView((null));
+
+        String textWithSpaces = userListView.withSpaces(name, 20);
+
+        assertEquals(20, textWithSpaces.length());//sprawdza czy dlugosc tekstu to dokładnie 20 znakow
+        assertTrue(textWithSpaces.startsWith(name));//sprawdza czy tekst zaczyna sie od imienia
+
+        String spacesAfterName = textWithSpaces.substring(name.length());
+        assertTrue(spacesAfterName.chars().allMatch(c -> c == ' '));//sprawdza czy wszystkie znaki po imieniu to spacje
+    }
+
 }
