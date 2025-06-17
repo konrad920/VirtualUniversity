a4e5face2b9e04ce1776d5f72b15beacef8caeca
konrad920 <128093250+konrad920@users.noreply.github.com>
2025-06-17 21:16:14 +0200

    instrukcja 3 - zadanie 1.5 dodanie klasy Subject

---
+++ src/main/java/pl/wsiz/model/Subject.java
@@ -0,0 +1,27 @@
+package pl.wsiz.model;
+
+public class Subject {
+    private String name;
+    private int ectsPoints;
+
+    public Subject(String name, int ectsPoints) {
+        this.name = name;
+        this.ectsPoints = ectsPoints;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEctsPoints() {
+        return ectsPoints;
+    }
+
+    public void setEctsPoints(int ectsPoints) {
+        this.ectsPoints = ectsPoints;
+    }
+}
